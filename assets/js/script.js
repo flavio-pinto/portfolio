88034e75c6d30837310fc4127df10a8537347d7a
Flavio Pinto <flaviopnt@hotmail.it>
2023-03-13 19:52:51 +0100

    [FEAT] Button active status script

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,8 @@ function sectionTransitions() {
   //Active button
   for(let i = 0; i < sectionControlBtns.length; i++) {
     sectionControlBtns[i].addEventListener('click', function() {
-      let currentBtn = document.querySelectorAll('.btn-active');
-      currentBtn[0].className = currentBtn[0].className.replace('btn-active', '');
+      let currentBtn = document.querySelector('.btn-active');
+      currentBtn.className = currentBtn.className.replace('btn-active', '');
       this.className += ' btn-active';
     });
   }

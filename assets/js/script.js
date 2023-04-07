3b1b67dff5a02aa111e3155aa085241a7f7633af
Flavio Pinto <flaviopnt@hotmail.it>
2023-04-07 19:41:08 +0200

    Refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 (function () {
   document.querySelectorAll(".control").forEach((button) => {
     button.addEventListener("click", function () {
-      document.querySelector(".active-btn").classList.remove("active-btn");
-      this.classList.add("active-btn");
+      document.querySelector(".active-ctrl").classList.remove("active-ctrl");
+      this.classList.add("active-ctrl");
       document.querySelector(".active").classList.remove("active");
       document.getElementById(button.dataset.id).classList.add("active");
     });

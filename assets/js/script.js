22b660f1a17138b60281971d851585731c03908b
Flavio Pinto <flaviopnt@hotmail.it>
2023-11-03 15:05:15 +0100

    [FIX] Personal links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 (function () {
   document.querySelectorAll(".control").forEach((button) => {
     button.addEventListener("click", function () {
-      document.querySelector(".active-ctrl").classList.remove("active-ctrl");
-      this.classList.add("active-ctrl");
-      document.querySelector(".active").classList.remove("active");
-      document.getElementById(button.dataset.id).classList.add("active");
-    });
-  });
+      document.querySelector(".active-ctrl").classList.remove("active-ctrl")
+      this.classList.add("active-ctrl")
+      document.querySelector(".active").classList.remove("active")
+      document.getElementById(button.dataset.id).classList.add("active")
+    })
+  })
   document.querySelector(".theme-switch").addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-  });
-})();
+    document.body.classList.toggle("light-mode")
+  })
+})()
